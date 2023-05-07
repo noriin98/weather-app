97050e4b6b52ad8a95ae370e433aa24fa33cdbed
noriin98 <125356694+noriin98@users.noreply.github.com>
2023-05-07 18:07:23 +0200

    m

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Weather from "./Weather";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+    <div className='container'>
+     <Weather />
+     <footer>
+     This project was coded by Noreen Akhtar and is open-sorced on Github</footer>
+     </div>
+     </div>
+  );
+}
+
---
+++ src/WeatherInfo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import FormattedDate from "./FormattedDate"; 
+
+export default function WeatherInfo(props){
+    return (
+        <div className="WeatherInfo">
+    <h1> {props.data.city} </h1>
+            <ul>
+            <li>
+            <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
+            </ul>
+            <div className="row mt-3">
+            <div className="col-6">
+            <img src={props.data.iconUrl} alt=""/>
+            <span className="temp">{Math.round(props.data.temperature)}</span>
+            <span className="unit">°C</span>
+            </div>
+            <div className="col-6">
+            <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: {props.data.humidity} %</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+            </ul>
+            </div>
+            </div>
+            </div>
+    );
+}
